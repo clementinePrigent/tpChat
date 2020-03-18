54788d020ceca1e43ca36e84ef0a8e1cf98df912
clementine <clementine.prigent2019@campus-eni.fr>
2020-03-18 16:02:04 +0100

    Initial commit

---
+++ Controllers/ChatController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TPChats.Controllers
+{
+    public class ChatController : Controller
+    {
+
+        private static List<TP.Models.Chat> chats;
+        public List<TP.Models.Chat> Chats => chats ?? (chats = TP.Models.Chat.GetMeuteDeChats());
+
+        // GET: Chat
+        public ActionResult Index()
+        {
+
+            return View(chats);
+        }
+
+        // GET: Chat/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Chat/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Chat/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TPChats.MvcApplication" Language="C#" %>
